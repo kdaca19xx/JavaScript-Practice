f1707459c970b4dfd683ef42d433f767d4b7b752
Kerstin Carson <160202375+kdaca19xx@users.noreply.github.com>
2024-10-22 13:54:17 -0500

    Head First JavaScript

---
+++ HFJS_Ch1_99 Bottles of Water.js
@@ -0,0 +1,14 @@
+let i = 99;
+
+while (i > 0) {
+  console.log(`${i} bottles of water on the wall. ${i} bottles of water.`);
+  console.log("You take one down and pass it around");
+  i = i - 1;
+
+if (i > 0) {
+  console.log(`${i} bottles of water on the wall.`);
+  
+} else {
+  console.log("No more bottles of water on the wall!");
+  }
+}
