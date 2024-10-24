4bdb406ffcca8389d3eb9c6c5ff40cd8263a1cdd
Kerstin Carson <160202375+kdaca19xx@users.noreply.github.com>
2024-10-23 22:22:35 -0500

    Codewars

---
+++ GrasshopperGradebook_Oct 15 2024.js
@@ -0,0 +1,16 @@
+function getGrade(s1, s2, s3) {
+ 
+  let averageGrade = (s1 + s2 + s3) / 3;
+
+  if (averageGrade >= 90) {
+    return 'A';
+  } else if (averageGrade >= 80) {
+    return 'B';
+  } else if (averageGrade >= 70) {
+    return 'C';
+  } else if (averageGrade >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
